588d379b273a1fd3e86b78ffb89ab4e2231ae346
Денис Котляров <denis2005991@gmail.com>
2019-10-17 20:18:17 +0300

    1. Preparation for first release.

--- src/lib.rs
+++ src/lib.rs
@@ -23,11 +23,56 @@
 /*!
 A safe but not complete implementation of the goto operator.
 
+# Attention!!
+1. At the moment, this is not a complete implementation of the goto operator.
+2. We do not plan to violate Rust's safety standards. We provide useful and interesting macros that partially (or fully) implement the goto operator.
+
+
+# gpoint:
+"GOTO point", allows you to return to this line later.
+
+```rust
+#[macro_use]
+extern crate goto;
+
+fn main() {
+	let data = b"1234567890";
+	let mut iter = data.iter();
+	let mut a;
+
+	gpoint!['begin:
+		a = iter.next();
+		match a {
+			a @ Some(b'0') if a == Some(&b'9') => {
+				println!("#a 0!");
+				
+				gpoint!['add:
+					
+				];
+			},
+			Some(a) => {
+				println!("#a {}", unsafe { std::char::from_u32_unchecked(*a as u32) });
+				continue 'begin;
+			},
+			_ => break 'begin,
+		}
+		
+	];
+}
+
+```
+
+# License
+
+Copyright 2019 #UlinProject (Denis Kotlyarov) Денис Котляров
+
+Licensed under the MIT License
+
 */
 
 #![no_std]
 
-
+/// "GOTO point", allows you to return to this line later.
 #[macro_export]
 macro_rules! gpoint {
 	[ $n:tt + $n2:tt + $n3:tt : $($tt: tt)* ] => {{
@@ -78,12 +123,5 @@ macro_rules! gpoint {
 			#[allow(unreachable_code)]
 			break $name
 		}
-	}};
-}
-
-#[macro_export]
-macro_rules! goto {
-	[ $name:tt ] => {{
-		continue $name
 	}};
 }
